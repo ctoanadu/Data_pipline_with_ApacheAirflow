b9319080b42aecc1db53c591fae81927e460259d
Chukwudi To-Anadu <ctoanadu@gmail.com>
2020-06-30 02:54:41 +0100

    Creating custom Airflow plugins

---
+++ plugins/operators/load_fact.py
@@ -0,0 +1,46 @@
+from airflow.hooks.postgres_hook import PostgresHook
+from airflow.models import BaseOperator
+from airflow.utils.decorators import apply_defaults
+
+class LoadFactOperator(BaseOperator):
+     ui_color = '#F98866'
+    copy_sql = """
+        {}
+        ACCESS_KEY_ID '{}'
+        SECRET_ACCESS_KEY '{}'
+        IGNOREHEADER {}
+        DELIMITER '{}'
+    """
+
+   
+   
+
+    @apply_defaults
+    def __init__(self,
+                 redshift_conn_id="",
+                 sql_query ="",
+                 aws_credentials_id="",
+                 delimiter="",
+                 ignore_headers=1,
+                 *args, **kwargs):
+
+        super(LoadFactOperator, self).__init__(*args, **kwargs)
+        self.redshift_conn_id=redshift_conn_id
+        self.sql_query=sql_query
+        self.aws_credentials_id=aws_credentials_id
+        self.delimiter=delimiter
+        self.ignore_headers=ignore_headers
+        
+    def execute(self, context):
+        aws_hook=AwsHook(self.aws_credentials_id)
+        credentials=aws_hook.get_credentials()
+        redshift=PostgresHook(postgres_conn_id=self.redshift_conn_id)   
+        
+        formatted_sql=LoadFactOperator.copy_sql.format(
+            self.sql_query,
+            credentials.access_key,
+            credentials.secret_key,
+            self.ignore_headers,
+            self.delimiter
+        )
+        redshift.run(formatted_sql)
